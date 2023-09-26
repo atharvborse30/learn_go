8deb9634fe3244bf9db1385d879a806f3914ae85
Atharv Borse <61404222+atharvborse30@users.noreply.github.com>
2023-09-27 00:08:27 +0530

    Add files via upload

---
+++ constants.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+const PI float64 = 3.14
+
+func main() {
+	var radius float63 = 5.0
+	var area float64
+	area = PI * radius * radius
+	fmt.Println("Area of circle is : ", area)
+}
