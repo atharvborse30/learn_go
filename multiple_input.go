61feb06e90903f6b0696b2e9ac0e7bb417223faf
Atharv Borse <61404222+atharvborse30@users.noreply.github.com>
2023-09-26 23:08:10 +0530

    Add files via upload

---
+++ multiple_input.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+	var is_muggle bool
+	fmt.Print("Enter you name & are you a muggle: ")
+	fmt.Scanf("%s %t", &name, &is_muggle)
+	fmt.Println(name, is_muggle)
+}
